11dc2173b9196e27558ab58cf118f935e80ddfb8
KimJaeYoung <kjyoffice@gmail.com>
2026-01-18 14:19:35 +0900

    Keep 코드로 있던 JS를 참조용이니 TS로 이동

--- UIOne/OneFileEncryptDecrypt.UIX/WebViewRoot_TS/DefaultJS.ts
+++ UIOne/OneFileEncryptDecrypt.UIX/WebViewRoot_TS/DefaultJS.ts
@@ -1,2 +1,19 @@
 ﻿// Hello
 
+
+
+/*
+    const HandShakeX = function () {
+        return chrome.webview.hostObjects.handShake;
+    };
+
+    const SendHelloMessage = async function () {
+        console.log(await HandShakeX().HelloMessage('Anders'));
+    };
+
+    window.chrome.webview.addEventListener('message',
+        function (e) {
+            console.log(e.data, e);
+        }
+    );
+*/
